8f37b74e13bdda2e11fa7bc350a52d70dd666942
TMNTturtleguy <kevin.thueringer@gmail.com>
2017-06-16 10:26:17 -0500

    v2 es-restart
    added automated es-restart thanks to cyperghost new code

--- change_es_systems.sh
+++ change_es_systems.sh
@@ -5,6 +5,30 @@
 #
 # v1
 # TMNTturtlguy - June 2017
+#
+# place script in /home/pi/RetroPie/retropiemenu/
+# In order to run the script you must create the following folders:
+#    /opt/retropie/configs/all/emulationstation/es_systems
+# Then within that folder create these 4 folders:
+#    all
+#    consoles
+#    customs
+#    favorites
+# Example of the full path: /opt/retropie/configs/all/emulationstation/es_systems/favorites
+# Within each folder you have created place a es_systems.cfg file. The file needs to be named the same in each folder, but the systems can be different.
+# Restart EmulationStation and the script is located in the retropie menu
+#
+# v2
+# EmulationStation Restart Script by cyperghost - June 2017
+#	Special thanks to cyperghost for helping find a great solution to restart ES from command line or script See forum post https://retropie.org.uk/forum/topic/11050/restart-es-via-bash-script
+#
+# 
+
+#scirpt welcome screen
+ dialog --backtitle "W A R N I N G !" --title " WARNING! " \
+    --yesno "\nThis script lets you change your es_systems.cfg file used to display systems.  This will not change or remove any systems or content, it will change which systems are displayed based on user created es_systems.cfg files.  ES will automatically restart.\n\n\nDo you want to proceed?" \
+    15 75 2>&1 > /dev/tty \
+    || exit
 
 # dialog functions ##########################################################
 
@@ -62,24 +86,52 @@ function change_to_all() {
     rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
 	
 	cp /opt/retropie/configs/all/emulationstation/es_systems/all/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+	
+	echo "Create es-restart"
+	sudo touch "/tmp/es-restart"
+	echo "Kill latest process PID of ES"
+	sleep 3
+	kill $(pgrep -l -n emulationstatio | awk '!/grep/ {printf "%s ",$1}')
+	sudo rm -f "/tmp/es-restart"
 }
 
 function change_to_consoles() {
     rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
 	
 	cp /opt/retropie/configs/all/emulationstation/es_systems/consoles/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+	
+	echo "Create es-restart"
+	sudo touch "/tmp/es-restart"
+	echo "Kill latest process PID of ES"
+	sleep 3
+	kill $(pgrep -l -n emulationstatio | awk '!/grep/ {printf "%s ",$1}')
+	sudo rm -f "/tmp/es-restart"
 }
 
 function change_to_customs() {
     rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
 	
 	cp /opt/retropie/configs/all/emulationstation/es_systems/customs/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+	
+	echo "Create es-restart"
+	sudo touch "/tmp/es-restart"
+	echo "Kill latest process PID of ES"
+	sleep 3
+	kill $(pgrep -l -n emulationstatio | awk '!/grep/ {printf "%s ",$1}')
+	sudo rm -f "/tmp/es-restart"
 }
 
 function change_to_favorites() {
     rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
 	
 	cp /opt/retropie/configs/all/emulationstation/es_systems/favorites/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+	
+	echo "Create es-restart"
+	sudo touch "/tmp/es-restart"
+	echo "Kill latest process PID of ES"
+	sleep 3
+	kill $(pgrep -l -n emulationstatio | awk '!/grep/ {printf "%s ",$1}')
+	sudo rm -f "/tmp/es-restart"
 }
 
 
