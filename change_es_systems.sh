cad517755e81f36be45bc5ca0469665b241e0fe5
meleu <meleu@users.noreply.github.com>
2017-06-19 07:36:29 -0300

    Update change_es_systems.sh

--- change_es_systems.sh
+++ change_es_systems.sh
@@ -20,43 +20,21 @@
 #
 # v2
 # EmulationStation Restart Script by cyperghost - June 2017
-#	Special thanks to cyperghost for helping find a great solution to restart ES from command line or script See forum post https://retropie.org.uk/forum/topic/11050/restart-es-via-bash-script
+#    Special thanks to cyperghost for helping find a great solution to restart ES from command line or script See forum post https://retropie.org.uk/forum/topic/11050/restart-es-via-bash-script
 #
 # 
 
-#scirpt welcome screen
- dialog --backtitle "W A R N I N G !" --title " WARNING! " \
-    --yesno "\nThis script lets you change your es_systems.cfg file used to display systems.  This will not change or remove any systems or content, it will change which systems are displayed based on user created es_systems.cfg files.  ES will automatically restart.\n\n\nDo you want to proceed?" \
+#script welcome screen
+dialog --backtitle "W A R N I N G !" --title " WARNING! " \
+    --yesno "\nThis script lets you change your es_systems.cfg file used to display systems.  This will not change or remove any systems or content, it will change which systems are displayed based on user created es_systems.cfg files. ES will automatically restart.\n\n\nDo you want to proceed?" \
     15 75 2>&1 > /dev/tty \
     || exit
 
-# dialog functions ##########################################################
 
-function dialogMenu() {
-    local text="$1"
-    shift
-    dialog --no-mouse \
-        --backtitle "$BACKTITLE" \
-        --cancel-label "Back" \
-        --ok-label "OK" \
-        --menu "$text\n\nChoose an option." 17 75 10 "$@" \
-        2>&1 > /dev/tty
+function restart_es() {
+    touch /tmp/es-restart \
+    && pkill -f "/opt/retropie/supplementary/.*/emulationstation([^.]|$)"
 }
-
-function dialogYesNo() {
-    dialog --no-mouse --backtitle "$BACKTITLE" --yesno "$@" 15 75 2>&1 > /dev/tty
-}
-
-function dialogMsg() {
-    dialog --no-mouse --ok-label "OK" --backtitle "$BACKTITLE" --msgbox "$@" 20 70 2>&1 > /dev/tty
-}
-
-function dialogInfo {
-    dialog --infobox "$@" 8 50 2>&1 >/dev/tty
-}
-
-# end of dialog functions ###################################################
-
 
 function main_menu() {
     local choice
@@ -84,57 +62,27 @@ function main_menu() {
 
 function change_to_all() {
     rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
-	
-	cp /opt/retropie/configs/all/emulationstation/es_systems/all/es_systems.cfg /opt/retropie/configs/all/emulationstation/
-	
-	echo "Create es-restart"
-	sudo touch "/tmp/es-restart"
-	echo "Kill latest process PID of ES"
-	sleep 3
-	kill $(pgrep -l -n emulationstatio | awk '!/grep/ {printf "%s ",$1}')
-	sudo rm -f "/tmp/es-restart"
+    cp /opt/retropie/configs/all/emulationstation/es_systems/all/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    restart_es
 }
 
 function change_to_consoles() {
     rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
-	
-	cp /opt/retropie/configs/all/emulationstation/es_systems/consoles/es_systems.cfg /opt/retropie/configs/all/emulationstation/
-	
-	echo "Create es-restart"
-	sudo touch "/tmp/es-restart"
-	echo "Kill latest process PID of ES"
-	sleep 3
-	kill $(pgrep -l -n emulationstatio | awk '!/grep/ {printf "%s ",$1}')
-	sudo rm -f "/tmp/es-restart"
+    cp /opt/retropie/configs/all/emulationstation/es_systems/consoles/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    restart_es
 }
 
 function change_to_customs() {
     rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
-	
-	cp /opt/retropie/configs/all/emulationstation/es_systems/customs/es_systems.cfg /opt/retropie/configs/all/emulationstation/
-	
-	echo "Create es-restart"
-	sudo touch "/tmp/es-restart"
-	echo "Kill latest process PID of ES"
-	sleep 3
-	kill $(pgrep -l -n emulationstatio | awk '!/grep/ {printf "%s ",$1}')
-	sudo rm -f "/tmp/es-restart"
+    cp /opt/retropie/configs/all/emulationstation/es_systems/customs/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    restart_es
 }
 
 function change_to_favorites() {
     rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
-	
-	cp /opt/retropie/configs/all/emulationstation/es_systems/favorites/es_systems.cfg /opt/retropie/configs/all/emulationstation/
-	
-	echo "Create es-restart"
-	sudo touch "/tmp/es-restart"
-	echo "Kill latest process PID of ES"
-	sleep 3
-	kill $(pgrep -l -n emulationstatio | awk '!/grep/ {printf "%s ",$1}')
-	sudo rm -f "/tmp/es-restart"
+    cp /opt/retropie/configs/all/emulationstation/es_systems/favorites/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    restart_es
 }
 
-
-# START HERE #################################################################
 
 main_menu
