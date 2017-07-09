438e5f406969701faef1c612a4aae0a8c8c190bc
TMNTturtleguy <kevin.thueringer@gmail.com>
2017-07-09 02:00:02 -0500

    V4
    Update to add auto restart thanks to @meleu and @ cyperghost.
    Addition of Neogeo Capcom system.

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 change_es_systems
 This script lets you change the es_systems.cfg file used to display systems using your controllers.
 
-v1
-TMNTturtlguy - June 2017
+v4
+TMNTturtlguy - July 9 2017
 
 place script in /home/pi/RetroPie/retropiemenu/
- In order to run the script you must create the following folders:
+In order to run the script you must create the following folders:
     /opt/retropie/configs/all/emulationstation/es_systems
- Then within that folder create these 4 folders:
+Then within that folder create these 6 folders:
     All
     Consoles
     Customs
     Favorites
+    Hacks
+    Neocapcom
  Example of the full path: /opt/retropie/configs/all/emulationstation/es_systems/Favorites
  Within each folder you have created place a es_systems.cfg file. The file needs to be named the same in each folder, but the systems can be different.
- Restart EmulationStation Manually and the script is located in the retropie menu
+ Emulationstation will automatically restart.
+ 
+ Special Thanks to @meleu and @cyperghost for their hard work helping trouble shoot this script and comming up with a way to restart ES.
 
  
--- change_es_systems.sh
+++ change_es_systems.sh
@@ -3,58 +3,36 @@
 #############
 # This script lets you change the es_systems.cfg file used to display systems using your controllers.
 #
-# v3
-# TMNTturtlguy - June 2017
+# v4
+# TMNTturtlguy - July 9 2017
 #
 # place script in /home/pi/RetroPie/retropiemenu/
 # In order to run the script you must create the following folders:
 #    /opt/retropie/configs/all/emulationstation/es_systems
-# Then within that folder create these 4 folders:
+# Then within that folder create these 6 folders:
 #    All
 #    Consoles
 #    Customs
 #    Favorites
 #    Hacks
+#    Neocapcom
 # Example of the full path: /opt/retropie/configs/all/emulationstation/es_systems/Favorites
 # Within each folder you have created place a es_systems.cfg file. The file needs to be named the same in each folder, but the systems can be different.
 # Restart EmulationStation and the script is located in the retropie menu
-#
+# Special Thanks to @meleu and @cyperghost for their hard work helping trouble shoot this script and comming up with a way to restart ES.
 # 
 # 
 
 #scirpt welcome screen
  dialog --backtitle "W A R N I N G !" --title " WARNING! " \
-    --yesno "\nThis script lets you change your es_systems.cfg file used to display systems.  This will not change or remove any systems or content, it will change which systems are displayed based on user created es_systems.cfg files.  You will need to manually restart ES after running the script.\n\n\nDo you want to proceed?" \
+    --yesno "\nThis script lets you change your es_systems.cfg file used to display systems.  This will not change or remove any systems or content, it will change which systems are displayed based on user created es_systems.cfg files.  ES will automatically restart.\n\n\nDo you want to proceed?" \
     15 75 2>&1 > /dev/tty \
     || exit
 
-# dialog functions ##########################################################
-
-function dialogMenu() {
-    local text="$1"
-    shift
-    dialog --no-mouse \
-        --backtitle "$BACKTITLE" \
-        --cancel-label "Back" \
-        --ok-label "OK" \
-        --menu "$text\n\nChoose an option." 17 75 10 "$@" \
-        2>&1 > /dev/tty
+function restart_es() {
+    touch /tmp/es-restart && pkill -f "/opt/retropie/supplementary/.*/emulationstation([^.]|$)"
+    exit
 }
-
-function dialogYesNo() {
-    dialog --no-mouse --backtitle "$BACKTITLE" --yesno "$@" 15 75 2>&1 > /dev/tty
-}
-
-function dialogMsg() {
-    dialog --no-mouse --ok-label "OK" --backtitle "$BACKTITLE" --msgbox "$@" 20 70 2>&1 > /dev/tty
-}
-
-function dialogInfo {
-    dialog --infobox "$@" 8 50 2>&1 >/dev/tty
-}
-
-# end of dialog functions ###################################################
-
 
 function main_menu() {
     local choice
@@ -63,11 +41,12 @@ function main_menu() {
         choice=$(dialog --backtitle "$BACKTITLE" --title " MAIN MENU " \
             --ok-label OK --cancel-label Exit \
             --menu "What do you want to do?" 17 75 10 \
-            B "Change to es_systems ALL" \
-            E "change to es_systems Consoles" \
-            C "Change to es_systems Customs" \
-            R "Change to es_systems Favorites" \
-			U "Change to es_systems Hacks" \
+            B "Change to es_systems ALL and restart ES" \
+            E "change to es_systems Consoles and restart ES" \
+            C "Change to es_systems Customs and restart ES" \
+            R "Change to es_systems Favorites and restart ES" \
+			U "Change to es_systems Hacks and restart ES" \
+			X "Chante to es_systems NeoGeo Capcom and restart ES" \
             2>&1 > /dev/tty)
 
         case "$choice" in
@@ -75,45 +54,48 @@ function main_menu() {
             E)  change_to_consoles ;;
             C)  change_to_customs ;;
             R)  change_to_favorites ;;
-			U)  change_to_hacks ;;
+			U) change_to_hacks ;;
+			X) change_to_neocapcom ;;
             *)  break ;;
         esac
     done
 }
 
+
 function change_to_all() {
-    sudo rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
-	
-    sudo cp /opt/retropie/configs/all/emulationstation/es_systems/All/es_systems.cfg /opt/retropie/configs/all/emulationstation/
-	
+    rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
+    cp /opt/retropie/configs/all/emulationstation/es_systems/All/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    restart_es
 }
 
 function change_to_consoles() {
-    sudo rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
-	
-    sudo cp /opt/retropie/configs/all/emulationstation/es_systems/Consoles/es_systems.cfg /opt/retropie/configs/all/emulationstation/
-
+    rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
+    cp /opt/retropie/configs/all/emulationstation/es_systems/Consoles/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    restart_es
 }
 
 function change_to_customs() {
-    sudo rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
-	
-    sudo cp /opt/retropie/configs/all/emulationstation/es_systems/Customs/es_systems.cfg /opt/retropie/configs/all/emulationstation/
-
+    rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
+    cp /opt/retropie/configs/all/emulationstation/es_systems/Customs/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    restart_es
 }
 
 function change_to_favorites() {
-    sudo rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
-	
-    sudo cp /opt/retropie/configs/all/emulationstation/es_systems/Favorites/es_systems.cfg /opt/retropie/configs/all/emulationstation/
-
+    rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
+    cp /opt/retropie/configs/all/emulationstation/es_systems/Favorites/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    restart_es
 }
 
 function change_to_hacks() {
-    sudo rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
-	
-    sudo cp /opt/retropie/configs/all/emulationstation/es_systems/Hacks/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
+    cp /opt/retropie/configs/all/emulationstation/es_systems/Hacks/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    restart_es
+}
 
+function change_to_neocapcom() {
+    rm /opt/retropie/configs/all/emulationstation/es_systems.cfg
+    cp /opt/retropie/configs/all/emulationstation/es_systems/Neocapcom/es_systems.cfg /opt/retropie/configs/all/emulationstation/
+    restart_es
 }
 
 # START HERE #################################################################
